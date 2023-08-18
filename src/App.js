8a4b2bc93068c914118fe830b4e1b894b7889abf
shivanshu-samadhiya <shivanshusamadhiya23@gmail.com>
2023-08-18 22:49:30 +0530

    react code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import {Header} from "./components/header";
+import Home from "./components/home";
+ function App() {
+   return (
+    <div> 
+      <Header />
+      <Home />
+      </div>
+   );
+ }
+ 
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+function Header() {
+    return<nav>Get Ready To complete your daily Course</nav>
+}
+export { Header}
